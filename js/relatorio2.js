6cb665cd9b2dc542b3cab9aaec4627d69ee22dea
Carlos <ocariocawebdesign@gmail.com>
2020-02-04 16:44:00 -0300

    Exclusão de arquivos e novas alterações.

--- js/relatorio2.js
+++ js/relatorio2.js
@@ -10,15 +10,18 @@
         var mediaSetima = (totalSetimaMes / totalReunioesMes);
         var mediaSetimaMembro = (totalSetimaMes / totalMembrosMes);
 
-        document.write("Relatório referente ao mês: " + relatorioMes + "<br>" +
+        document.write(
+            "<h1>Relatório Mensal</h1><br>" +
+            "Relatório referente ao mês: " + relatorioMes + "<br>" +
             "Total de reuniões = " + totalReunioesMes + "<br>" +
             "Total de membros = " + totalMembrosMes + "<br>" +
-            "Total da sétima  = " + totalSetimaMes + "<br>" +
+            "Total da sétima  = " + totalSetimaMes.toFixed(2) + "<br>" +
             "Total de visitantes no mês = " + totalVisitantesMes + "<br>" +
             "Total de ingressos = " + totalIngressosMes + "<br>" +
-            "Saldo anterior = " + saldoAnterior + "<br>" +
-            "Saldo atual é = " + (saldoAnterior + totalSetimaMes) + "<br>" +
-            "<br>A média de sétima mensal é: R$" + mediaSetima + "<br>"
+            "Saldo anterior R$" + saldoAnterior.toFixed(2) + "<br>" +
+            "Saldo atual é R$" + (saldoAnterior + totalSetimaMes) + "<br>" +
+            "<br>A média de sétima mensal é: R$" + mediaSetima.toFixed(2) + "<br>" +
+            "Média de sétima por membros é R$" + mediaSetimaMembro.toFixed(2) + "<br>"
 
         )
 
