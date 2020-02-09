a96dd5522250f653cc7a5393b02440d7baa0e4af
Carlos <ocariocawebdesign@gmail.com>
2020-02-09 19:11:47 -0300

    Novas alterações em campos.

---
+++ js/literatura.js
@@ -0,0 +1,14 @@
+var literatura = ["texto Básico", "isto Resulta", "triangulo Da AutoAceitação", "só Por Hoje"];
+
+var literatura = {
+
+    nome: literatura[0],
+
+
+}
+
+
+console.log(literatura[1]);
+console.log(literatura[2]);
+console.log(literatura[3]);
+console.log(literatura[4]);
--- js/relatorio2.js
+++ js/relatorio2.js
@@ -1,87 +1,53 @@
+function criarRelatorio() {
 
-    function criarRelatorio() {
+    //Novas variaveis
+    //Servidores
 
-        var conteudoForm = document.getElementById("conteudoForm");
-        var relatorioMes = mesDoRelatorio.value;
-        var totalReunioesMes = parseInt(document.getElementById("reunioesMes").value);
-        var totalMembrosMes = parseInt(document.getElementById("membrosNoMes").value);
-        var totalSetimaMes = parseFloat(document.getElementById("totalSetimaDoMes").value);
-        var totalVisitantesMes = parseInt(document.getElementById("totalDeVisitantes").value);
-        var totalIngressosMes = parseInt(document.getElementById("totalDeIngressos").value);
-        var saldoAnterior = parseFloat(document.getElementById("saldoAnterior").value);
-        var mediaSetima = (totalSetimaMes / totalReunioesMes);
-        var mediaSetimaMembro = (totalSetimaMes / totalMembrosMes);
-
-        document.write(
-            "<h1>Relatório Mensal</h1><br>" +
-            "Relatório referente ao mês: " + relatorioMes + "<br>" +
-            "Total de reuniões = " + totalReunioesMes + "<br>" +
-            "Total de membros = " + totalMembrosMes + "<br>" +
-            "Total da sétima  = " + totalSetimaMes.toFixed(2) + "<br>" +
-            "Total de visitantes no mês = " + totalVisitantesMes + "<br>" +
-            "Total de ingressos = " + totalIngressosMes + "<br>" +
-            "Saldo anterior R$" + saldoAnterior.toFixed(2) + "<br>" +
-            "Saldo atual é R$" + (saldoAnterior + totalSetimaMes) + "<br>" +
-            "<br>A média de sétima mensal por reuniões é R$" + mediaSetima.toFixed(2) + "<br>" +
-            "Média de sétima por membro é R$" + mediaSetimaMembro.toFixed(2) + "<br>"
-
-        )
-
-    }
+    /*var anoDoRelatorio = "";
+    var nomeDoGrupo = "";
+    var secretario = "";
+    var tesoureiro = "";
+    var rsg = "";
+    var rsgSuplente = "";*/
 
 
+    //var conteudoForm = document.getElementById("conteudoForm");
+    var ano = anoDoRelatorio.value;
+    var nome = nomeDoGrupo.value;
+    var secretario = secretarioDoGrupo.value;
+    var tesoureiro = tesoureiroDoGrupo.value;
+    var rsg = rsgDoGrupo.value;
+    var rsgSuplente = rsgSuplenteDoGrupo.value;
 
-    /*
+    var relatorioMes = mesDoRelatorio.value;
+    var totalReunioesMes = parseInt(document.getElementById("reunioesMes").value);
+    var totalMembrosMes = parseInt(document.getElementById("membrosNoMes").value);
+    var totalSetimaMes = parseFloat(document.getElementById("totalSetimaDoMes").value);
+    var totalVisitantesMes = parseInt(document.getElementById("totalDeVisitantes").value);
+    var totalIngressosMes = parseInt(document.getElementById("totalDeIngressos").value);
+    var saldoAnterior = parseFloat(document.getElementById("saldoAnterior").value);
+    var mediaSetima = (totalSetimaMes / totalReunioesMes);
+    var mediaSetimaMembro = (totalSetimaMes / totalMembrosMes);
 
+    document.write(
+        "<h1>Relatório Mensal</h1><br>" +
+        "Nome do grupo: " + nome + "<hr>" +
+        "Relatório referente ao mês: " + relatorioMes + "<hr>" +
+        "Ano do relatório: " + ano + "<hr>" +
+        "Nome do secretário: " + secretario + "<hr>" +
+        "Nome do tesoureiro: " + tesoureiro + "<hr>" +
+        "Nome do rsg: " + rsg + "<hr>" +
+        "Nome do rsg suplente: " + rsgSuplente + "<hr>" +
 
-    var relatorioMes = document.getElementById("mesDoRelatorio").value;
-        //document.write("Relatório referente ao mês: " + relatorioMes + "<br>");
-
-        var totalReunioesMes = parseInt(document.getElementById("reunioesMes")).value;
-        //var totalReunioesMes = parseInt(document.getElementById("reunioesMes"));
-        //var totalReunioesMes2 = parseInt(totalReunioesMes);
-        //document.write("Total de reuniões = " + totalReunioesMes + "<br>");
-        var totalMembrosMes = parseInt(document.getElementById("membrosNoMes"));
-        //var totalMembrosMes = parseInt(totalMembrosMes);
-        //document.write("Total de membros = " + totalMembrosMes + "<br>");
-
-        var totalSetimaMes = parseFloat(document.getElementById("totalSetimaDoMes"));
-        //var totalSetimaMes = parseFloat(totalSetimaMes);
-        //document.write("Total da sétima  = " + totalSetimaMes + "<br>");
-
-        var totalVisitantesMes = parseInt(document.getElementById("totalDeVisitantes"));
-        //var totalVisitantesMes = parseFloat(totalVisitantesMes);
-        //document.write("Total de visitantes no mês = " + totalVisitantesMes + "<br>");
-
-        var totalIngressosMes = parseInt(document.getElementById("totalDeIngressos"));
-        //var totalIngressosMes = parseInt(totalIngressosMes);
-        //document.write("Total de ingressos = " + totalIngressosMes + "<br>");
-
-        var saldoAnterior = parseFloat(document.getElementById("saldoAnterior"));
-        //var saldoAnterior = parseFloat(saldoAnterior);
-        //document.write("Saldo anterior = " + saldoAnterior + "<br>");
-
-        var mediaSetima = (totalSetimaMes / totalReunioesMes);
-        //document.write("<br>A média de sétima mensal é: R$" + mediaSetima + "<br>");
-
-        var mediaSetimaMembro = (totalSetimaMes / totalMembrosMes);
-        //document.write("Total de sétima por membros é R$" + mediaSetimaMembro + "<br>");
-
-        //document.write("Saldo atual é = " + (saldoAnterior + totalSetimaMes) + "<br>");
-
-        //document.write("Saldo atual é = " + (saldoAnterior + totalSetimaMes) + "<br>");
+        "Total de reuniões = " + totalReunioesMes + "<hr>" +
+        "Total de membros = " + totalMembrosMes + "<hr>" +
+        "Total da sétima  = " + totalSetimaMes.toFixed(2) + "<hr>" +
+        "Total de visitantes no mês = " + totalVisitantesMes + "<hr>" +
+        "Total de ingressos = " + totalIngressosMes + "<hr>" +
+        "Saldo anterior R$" + saldoAnterior.toFixed(2) + "<hr>" +
+        "Saldo atual é R$" + (saldoAnterior + totalSetimaMes) + "<hr>" +
+        "<br>A média de sétima mensal por reuniões é R$" + mediaSetima.toFixed(2) + "<hr>" +
+        "Média de sétima por membro é R$" + mediaSetimaMembro.toFixed(2) + "<br>")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-    */
+}
