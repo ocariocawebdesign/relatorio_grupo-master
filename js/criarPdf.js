bfc8f6285137e4c66a23c43a988a9362d6db6c07
Carlos <ocariocawebdesign@gmail.com>
2020-02-23 16:41:16 -0300

    Correções no código index.html

---
+++ js/criarPdf.js
@@ -0,0 +1,6 @@
+function criarPdf() {
+
+    var doc = new jsPDF();
+
+
+}
--- js/relatorio2.js
+++ js/relatorio2.js
@@ -1,15 +1,5 @@
 function criarRelatorio() {
 
-    //Novas variaveis
-    //Servidores
-
-    /*var anoDoRelatorio = "";
-    var nomeDoGrupo = "";
-    var secretario = "";
-    var tesoureiro = "";
-    var rsg = "";
-    var rsgSuplente = "";*/
-
 
     //var conteudoForm = document.getElementById("conteudoForm");
     var ano = anoDoRelatorio.value;
@@ -49,5 +39,43 @@ function criarRelatorio() {
         "<br>A média de sétima mensal por reuniões é R$" + mediaSetima.toFixed(2) + "<hr>" +
         "Média de sétima por membro é R$" + mediaSetimaMembro.toFixed(2) + "<br>")
 
+}
+
+
+//https://www.sejda.com/html-to-pdf conversor html pdf
+
+function relatorioTeste() {
+
+    //var docRelatorio = new Array[ano, nome, secretario];
+
 
 }
+
+//|ARRAY VETOR
+var docRelatorio = ['ano', 'nome', 'secretario'];
+console.log(docRelatorio);
+
+
+
+/*var criarRelatorio = new jsPDF()
+
+document.write(
+    "<h1>Relatório Mensal</h1><br>" +
+    "Nome do grupo: " + nome + "<hr>" +
+    "Relatório referente ao mês: " + relatorioMes + "<hr>" +
+    "Ano do relatório: " + ano + "<hr>" +
+    "Nome do secretário: " + secretario + "<hr>" +
+    "Nome do tesoureiro: " + tesoureiro + "<hr>" +
+    "Nome do rsg: " + rsg + "<hr>" +
+    "Nome do rsg suplente: " + rsgSuplente + "<hr>" +
+
+    "Total de reuniões = " + totalReunioesMes + "<hr>" +
+    "Total de membros = " + totalMembrosMes + "<hr>" +
+    "Total da sétima  = " + totalSetimaMes.toFixed(2) + "<hr>" +
+    "Total de visitantes no mês = " + totalVisitantesMes + "<hr>" +
+    "Total de ingressos = " + totalIngressosMes + "<hr>" +
+    "Saldo anterior R$" + saldoAnterior.toFixed(2) + "<hr>" +
+    "Saldo atual é R$" + (saldoAnterior + totalSetimaMes) + "<hr>" +
+    "<br>A média de sétima mensal por reuniões é R$" + mediaSetima.toFixed(2) + "<hr>" +
+    "Média de sétima por membro é R$" + mediaSetimaMembro.toFixed(2) + "<br>")
+    */
